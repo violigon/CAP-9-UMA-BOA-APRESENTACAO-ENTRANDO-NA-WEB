683fbd8851ade16cd742ac66001b2b5fb4fe6505
Vinícius Oliveira Gonçalves <vinicius@oligon.com.br>
2022-02-27 22:33:57 -0300

    feat(index): implement mail function

---
+++ js/index.js
@@ -0,0 +1,23 @@
+const form = $("#formulario > form");
+
+form.on("submit", async event => {
+
+    event.preventDefault();
+    const body = form.serializeArray().reduce(function(obj, item) {
+        obj[item.name] = item.value;
+        return obj;
+    }, {});
+
+    const request = await fetch("https://backend-flexivel.herokuapp.com/", {
+        method: "POST",
+        body: JSON.stringify(body),
+        headers: new Headers({
+            Authorization: 'rm86538@fiap.com.br',
+            'Content-Type': 'application/json',
+        }),
+    });
+
+    const json = await request.json();
+
+    console.log({ json });
+});
